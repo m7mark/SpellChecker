a41b82d5a16b25884e2ade5fc2ef507cfbc1895f
m7mark <m7mark@yandex.ru>
2021-12-05 21:27:31 +0300

    upload file

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,64 @@
-// const http = require('http')
 import http from 'http';
 import formidable from 'formidable';
 import fs from 'fs'
-import fetch from 'node-fetch';
+import SpellChecker from './service'
+import EventEmitter from 'events';
+import { writeFile } from 'fs';
 
-
-const highWaterMark = 10 * 1024
+//options
+const highWaterMark = 3 * 512
 const encoding = 'utf-8'
 const PORT = 5000
+const emitter = new EventEmitter()
+const form = formidable();
 
 const server = http.createServer((req, res) => {
-  // res.writeHead(200, { 'Content-type': 'text/html; charset=utf-8' })
-  // if (req.url === '/api/spell') {
-  //   console.log(req);
-  //   res.end('spell')
-  // }
-  // else {
-  //   res.end()
-  // }
+
   if (req.url === '/api/upload' && req.method?.toLowerCase() === 'post') {
     // parse a file upload
-    const form = formidable();
-    form.on('file', function (name, file) {
+    form.on('file', async function (name, file) {
       if (file.mimetype !== 'text/plain') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify('error type'));
       }
-
-      // fs.readFile(file.filepath, 'utf-8', (e, data) => {
-      //   console.log(data);
-      // })
-
+      //create readeble stream from text file
       const stream = fs.createReadStream(file.filepath, { encoding, highWaterMark })
-      stream.on('data', (chank) => {
-        console.log(chank);
-        // fetch('https://github.com/')
-        //   .then(res => res.text())
-        //   .then(body => console.log(body));
-        // console.log(res);
-      })
+      let chunks = [] as Array<String>;
+      //every chunc check spell
+      for await (const chunk of stream) {
+        const response = await SpellChecker.sendChankText(chunk.toString())
+        chunks = chunks.concat(response);
+        // console.log('iteration');
+      }
+      //save new text in file and emmit action
+      writeFile('files/final.txt', chunks.join(), 'utf8', (err) => {
+        if (err) throw err;
+        emitter.emit('upload');
+      });
+
       stream.on('error', (e) => {
         console.log(e);
       })
-
     })
+
     form.parse(req, (err, fields, files) => {
       if (err) {
         res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
         res.end(String(err));
         return;
       }
-
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ fields, files }, null, 2));
+      emitter.once('upload', () => {
+        const filePath = 'files/final.txt'
+        if (fs.existsSync(filePath)) {
+          fs.createReadStream('files/final.txt').pipe(res)
+          return
+        }
+        res.writeHead(200, { 'Content-Type': 'text/plane' });
+        res.end('Error sending file');
+      });
     });
-
   }
-
-
-  // // show a file upload form
-  // res.writeHead(200, { 'Content-Type': 'text/html' });
-  // res.end(`
-  //   <h2>With Node.js <code>"http"</code> module</h2>
-  //   <form action="/api/upload" enctype="multipart/form-data" method="post">
-  //     <div>Text field title: <input type="text" name="title" /></div>
-  //     <div>File: <input type="file" name="multipleFiles" multiple="multiple" /></div>
-  //     <input type="submit" value="Upload" />
-  //   </form>
-  // `);
+  else {res.end()}
 })
 
 server.listen(PORT, () => console.log(`server start on ${PORT}`))
