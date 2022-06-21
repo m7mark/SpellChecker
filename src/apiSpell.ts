1a3d8fd2469c80b98318f742256538f7a3473a58
m7mark <m7mark@yandex.ru>
2022-06-22 02:07:17 +0300

    types

--- src/apiSpell.ts
+++ src/apiSpell.ts
@@ -1,30 +1,29 @@
-import FormData from 'form-data';
-import fetch from 'node-fetch';
+import FormData from 'form-data'
+import fetch from 'node-fetch'
 
 export const apiSpell = async (textPart: string) => {
-
   let tempText = ''
   let formData = new FormData()
-  formData.append('text', textPart);
+  formData.append('text', textPart)
 
   //changing wrong words
   const newText = (spellPositionArray: Array<Object>, text: string) => {
     tempText = text
     spellPositionArray.forEach((elem: any) => {
-      tempText = tempText.replace(
-        elem['word'],
-        elem['s'][0] || elem['word']
-      )
+      tempText = tempText.replace(elem['word'], elem['s'][0] || elem['word'])
     })
   }
   // post req to Yansex speller
   if (textPart.length) {
     try {
-      let response = await fetch('https://speller.yandex.net/services/spellservice.json/checkText', { method: 'POST', body: formData })
-      const spellPositionArray = await response.json();
+      let response = await fetch(
+        'https://speller.yandex.net/services/spellservice.json/checkText',
+        { method: 'POST', body: formData }
+      )
+      const spellPositionArray = (await response.json()) as Array<Object>
       newText(spellPositionArray, textPart)
     } catch (e) {
-      console.log('Yandex error: ' + e);
+      console.log('Yandex error: ' + e)
     }
   }
   return tempText
